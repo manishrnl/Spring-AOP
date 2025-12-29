d855e289a13937b973ba9a232c905b853c004dca
MANISH <manishrajrnl@gmail.com>
2025-12-29 19:23:03 +0530

    Part 2

--- src/main/java/springaop/aspects/LoggingAspects.java
+++ src/main/java/springaop/aspects/LoggingAspects.java
@@ -1,7 +1,44 @@
 package springaop.aspects;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
+@Slf4j
 public class LoggingAspects {
+
+
+    //  @Before("execution(* trackPackage(..))")
+//    @Before("execution(* springaop.services.impl.ShipmentServiceImpl.orderPackage(..))")
+    @Before("execution(* springaop.services.impl.*.*(..))")
+    public void beforeOrderPackage(JoinPoint joinPoint) {
+        log.info("Before orderPackage called from LoggingAspects kind : {}",
+                joinPoint.getKind());
+        log.info("Before orderPackage called from LoggingAspects Signature : {}",
+                joinPoint.getSignature());
+    }
+
+
+    //    @Before("within(springaop.services.impl.*)")
+    @Before("within(springaop..*) || transactionalPointCut()")
+    public void beforeServiceImplCall() {
+        log.info("Service Impl called");
+    }
+
+    @Before("transactionalPointCut()")
+    public void beforeTransactionalAnnotation() {
+        log.info("Transactional Annotation is used");
+    }
+
+
+    //Creating a custom pointCut that can be reused again which is stored inside a variable
+    @Pointcut("@annotation(org.springframework.transaction.annotation.Transactional)")
+    public void transactionalPointCut() {
+    }
+
 }
--- src/test/java/springaop/SpringAopApplicationTests.java
+++ src/test/java/springaop/SpringAopApplicationTests.java
@@ -1,13 +1,23 @@
 package springaop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import springaop.services.ShipmentService;
 
 @SpringBootTest
 class SpringAopApplicationTests {
+    @Autowired
+    private ShipmentService shipmentService;
 
     @Test
-    void contextLoads() {
+    void orderPackage() {
+        shipmentService.orderPackage(1L);
+    }
+
+    @Test
+    void trackPackage() {
+        shipmentService.trackPackage(1L);
     }
 
 }
